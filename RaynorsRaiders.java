2c76931ef17e429c7d2df107e032caa0cc119d62
amleija <antonio.m.leija@gmail.com>
2013-10-30 15:57:55 -0700

    Moving build code to b_m from RR

--- RaynorsRaiders.java
+++ RaynorsRaiders.java
@@ -16,6 +16,10 @@ public class RaynorsRaiders implements BWAPIEventListener {
 	int homePositionX, homePositionY, mainMins;
 	
 	JNIBWAPI bwapi;
+	/* Name AIs here */
+	core_reactive ai_core;
+	build_manager ai_builder;
+	
 	public static void main(String[] args) {
 		new RaynorsRaiders();
 	}
@@ -24,160 +28,11 @@ public class RaynorsRaiders implements BWAPIEventListener {
 		bwapi = new JNIBWAPI(this);
 		bwapi.start();
 	} 
-
-        // input: upgrade(UpgradeTypes.UPGRADE.ordinal()) where UPGRADE is the upgrade type
-        // method will do resource check before attempting upgrade
-        // so far, will only upgrade at base price
-	public void upgrade(int lvlup) {
-		UpgradeType gear = bwapi.getUpgradeType(lvlup);
-		
-		if(bwapi.getSelf().getMinerals() >= gear.getMineralPriceBase() && bwapi.getSelf().getGas() >= gear.getGasPriceBase()) {
-			for(Unit unit : bwapi.getMyUnits()) {
-				if(unit.getTypeID() == gear.getWhatUpgradesTypeID()) {
-					bwapi.upgrade(unit.getID(), gear.getID());
-				}
-			}
-		}
-	}
-
-        // input: TechTypes.TECHNOLOGY.ordinal() where TECHNOLOGY is the tech to research
-        // method will do resource check before attempting research
-	public void research(int item) {
-		TechType tech = bwapi.getTechType(item);
-		
-		if(bwapi.getSelf().getMinerals() >= tech.getMineralPrice() && bwapi.getSelf().getGas() >= tech.getGasPrice()) {
-			for(Unit unit : bwapi.getMyUnits()) {
-				if(unit.getTypeID() == tech.getWhatResearchesTypeID()) {
-					bwapi.research(unit.getID(), tech.getID());
-				}
-			}
-		}
-	}
-	
-        // input: build(UnitTypes.BUILDING.ordinal()) where BUILDING is the building to build
-        // will build near similar building types
-        // will also build add-ons (not sure if structure will attempt relocating if there's not enough room)
-	public void build(int structure) {
-		UnitType bldg = bwapi.getUnitType(structure);
-		
-		if (bwapi.getSelf().getMinerals() >= bldg.getMineralPrice() && bwapi.getSelf().getGas() >= bldg.getGasPrice()) {
-			if(bldg.isAddon()) {
-				//find parent structure
-				for(Unit unit : bwapi.getMyUnits()) {
-					if(unit.getTypeID() == bldg.getWhatBuildID()) {
-						bwapi.buildAddon(unit.getID(), bldg.getID());
-						break;
-					}
-				}
-				
-			} else {
-			// try to find the worker near our home position
-				int worker = getNearestUnit(UnitTypes.Terran_SCV.ordinal(), homePositionX, homePositionY);
-				if (worker != -1) {
-				// if we found him, try to select appropriate build tile position for bldg (near our home base)
-					int xtile = homePositionX, ytile = homePositionY;
-				
-					for(Unit unit : bwapi.getMyUnits()) {
-						if(unit.getTypeID() == bldg.getID()) {
-							xtile = unit.getX();
-							ytile = unit.getY();
-							break;
-						}
-					}
-				    Point buildTile = getBuildTile(worker, bldg.getID(), xtile, ytile);
-				    // if we found a good build position, and we aren't already constructing a bldg 
-				    // order our worker to build it
-				    if ((buildTile.x != -1) && (!weAreBuilding(bldg.getID()))) {
-				    	bwapi.build(worker, buildTile.x, buildTile.y, bldg.getID());
-				    }
-				}
-			}
-		}
-	}
-    // Returns the id of a unit of a given type, that is closest to a pixel position (x,y), or -1 if we
-    // don't have a unit of this type
-    public int getNearestUnit(int unitTypeID, int x, int y) {
-    	int nearestID = -1;
-	    double nearestDist = 9999999;
-	    for (Unit unit : bwapi.getMyUnits()) {
-	    	if ((unit.getTypeID() != unitTypeID) || (!unit.isCompleted())) continue;
-	    	double dist = Math.sqrt(Math.pow(unit.getX() - x, 2) + Math.pow(unit.getY() - y, 2));
-	    	if (nearestID == -1 || dist < nearestDist) {
-	    		nearestID = unit.getID();
-	    		nearestDist = dist;
-	    	}
-	    }
-	    return nearestID;
-    }	
-	
-	// Returns the Point object representing the suitable build tile position
-	// for a given building type near specified pixel position (or Point(-1,-1) if not found)
-	// (builderID should be our worker)
-	public Point getBuildTile(int builderID, int buildingTypeID, int x, int y) {
-		Point ret = new Point(-1, -1);
-		int maxDist = 3;
-		int stopDist = 40;
-		int tileX = x/32; int tileY = y/32;
-		
-		// Refinery, Assimilator, Extractor
-		if (bwapi.getUnitType(buildingTypeID).isRefinery()) {
-			for (Unit n : bwapi.getNeutralUnits()) {
-				if ((n.getTypeID() == UnitTypes.Resource_Vespene_Geyser.ordinal()) && 
-						( Math.abs(n.getTileX()-tileX) < stopDist ) &&
-						( Math.abs(n.getTileY()-tileY) < stopDist )
-						) return new Point(n.getTileX(),n.getTileY());
-			}
-		}
-		
-		while ((maxDist < stopDist) && (ret.x == -1)) {
-			for (int i=tileX-maxDist; i<=tileX+maxDist; i++) {
-				for (int j=tileY-maxDist; j<=tileY+maxDist; j++) {
-					if (bwapi.canBuildHere(builderID, i, j, buildingTypeID, false)) {
-						// units that are blocking the tile
-						boolean unitsInWay = false;
-						for (Unit u : bwapi.getAllUnits()) {
-							if (u.getID() == builderID) continue;
-							if ((Math.abs(u.getTileX()-i) < 4) && (Math.abs(u.getTileY()-j) < 4)) unitsInWay = true;
-						}
-						if (!unitsInWay) {
-							ret.x = i; ret.y = j;
-							return ret;
-						}
-						// creep for Zerg (this may not be needed - not tested yet)
-						if (bwapi.getUnitType(buildingTypeID).isRequiresCreep()) {
-							boolean creepMissing = false;
-							for (int k=i; k<=i+bwapi.getUnitType(buildingTypeID).getTileWidth(); k++) {
-								for (int l=j; l<=j+bwapi.getUnitType(buildingTypeID).getTileHeight(); l++) {
-									if (!bwapi.hasCreep(k, l)) creepMissing = true;
-									break;
-								}
-							}
-							if (creepMissing) continue; 
-						}
-						// psi power for Protoss (this seems to work out of the box)
-						if (bwapi.getUnitType(buildingTypeID).isRequiresPsi()) {}
-					}
-				}
-			}
-			maxDist += 2;
-		}
-		
-		if (ret.x == -1) bwapi.printText("Unable to find suitable build position for "+bwapi.getUnitType(buildingTypeID).getName());
-		return ret;
-	}
-	
-	// Returns true if we are currently constructing the building of a given type.
-	public boolean weAreBuilding(int buildingTypeID) {
-		for (Unit unit : bwapi.getMyUnits()) {
-			if ((unit.getTypeID() == buildingTypeID) && (!unit.isCompleted())) return true;
-			if (bwapi.getUnitType(unit.getTypeID()).isWorker() && unit.getConstructingTypeID() == buildingTypeID) return true;
-		}
-		return false;
-	}
-
 	
 	
-	public void connected() {}	
+	public void connected() {
+		bwapi.loadTypeData();
+	}	
 	public void gameStarted() {}
 	public void gameUpdate() 
 	{
